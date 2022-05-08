9e912ce9c517854ce1be69c020d5efbeb8d35797
Angelo Leonardi <dignitysquared@gmail.com>
2022-05-08 08:56:53 -0700

    Implement trash can functionality

---
+++ Assets/Scripts/Interaction/Containers/Trash.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjects;
+using UnityEngine;
+
+namespace Interaction {
+public class Trash : Container
+{
+    protected override void Interact() { }
+    protected override void Extract() { }
+    protected override void Insert()
+    {
+        Inventory.RemoveItem();
+    }
+}
+
+}
